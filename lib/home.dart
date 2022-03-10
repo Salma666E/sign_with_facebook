550a6d5d1b8b56ecde6d1f952165aa086c211b2f
Salma Essam <smsmacs666@gmail.com>
2022-03-10 12:19:27 +0200

    build home screen

---
+++ lib/home.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _isLoggedIn = false;
+  Map _userObj = {};
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login by Facebook"),
+      ),
+      body: Container(
+        child: _isLoggedIn
+            ? Column(
+                children: [
+                  Image.network(_userObj["picture"]["data"]["url"]),
+                  Text(_userObj["name"]),
+                  Text(_userObj["email"]),
+                  TextButton(
+                      onPressed: () {
+                        FacebookAuth.instance.logOut().then((value) {
+                          setState(() {
+                            _isLoggedIn = false;
+                            _userObj = {};
+                          });
+                        });
+                      },
+                      child: const Text("Logout"))
+                ],
+              )
+            : Center(
+                child: ElevatedButton(
+                  child: const Text("Login with Facebook"),
+                  onPressed: () async {
+                    FacebookAuth.instance.login(
+                        permissions: ["public_profile", "email"]).then((value) {
+                      FacebookAuth.instance.getUserData().then((userData) {
+                        setState(() {
+                          _isLoggedIn = true;
+                          _userObj = userData;
+                        });
+                      });
+                    }).then((value) {
+                      print('SSSSSSSSSSSSSSSSSSS');
+                    });
+                  },
+                ),
+              ),
+      ),
+    );
+  }
+}
