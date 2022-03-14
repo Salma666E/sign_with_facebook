0a3f2e20ff0da03a45f0ecfda9796edc3041668e
Salma Essam <smsmacs666@gmail.com>
2022-03-14 13:33:30 +0200

    finished login by facebook

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,122 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'home.dart';
+import 'dart:convert';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(LoginPage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isLoggedIn = false;
+  var profileData;
+
+  var facebookLogin = FacebookLogin();
+
+  void onLoginStatusChanged(bool isLoggedIn, {profileData}) {
+    setState(() {
+      this.isLoggedIn = isLoggedIn;
+      print('isLoggedIn: ' + isLoggedIn.toString());
+      this.profileData = profileData;
+      print('profileData: ' + profileData.toString());
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Facebook',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Facebook Login"),
+        ),
+        body: Center(
+          child: isLoggedIn
+              ? _displayUserData(profileData)
+              : _displayLoginButton(),
+        ),
       ),
-      home: const HomePage(),
     );
+  }
+
+  void loginButtonClicked() async {
+    var facebookLoginResult = await facebookLogin.logIn(['email']);
+    switch (facebookLoginResult.status) {
+      case FacebookLoginStatus.error:
+        onLoginStatusChanged(false);
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        onLoginStatusChanged(false);
+        break;
+      case FacebookLoginStatus.loggedIn:
+        print('facebookLoginResult' +
+            facebookLoginResult.accessToken.token.toString());
+        var graphResponse = await http.get(Uri.parse(
+            'https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email,picture.height(200)&access_token=${facebookLoginResult.accessToken.token}'));
+
+        var profile = json.decode(graphResponse.body);
+        print('profile: ' + profile.toString());
+
+        onLoginStatusChanged(true, profileData: profile);
+        break;
+    }
+  }
+
+  _displayUserData(profileData) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          height: 200.0,
+          width: 200.0,
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                profileData['picture']['data']['url'],
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 28.0),
+        Text(
+          "Name: ${profileData['name']}",
+          style: const TextStyle(
+            fontSize: 20.0,
+          ),
+        ),
+        Text(
+          "Email: ${profileData['email']}",
+          style: const TextStyle(
+            fontSize: 20.0,
+          ),
+        ),
+        ElevatedButton(
+          child: const Text("Logout"),
+          onPressed: () => facebookLogin.isLoggedIn
+              .then((isLoggedIn) => isLoggedIn ? _logout() : {}),
+        )
+      ],
+    );
+  }
+
+  _displayLoginButton() {
+    return ElevatedButton(
+      child: const Text("Login with Facebook"),
+      onPressed: () => loginButtonClicked(),
+    );
+  }
+
+  _logout() async {
+    await facebookLogin.logOut();
+    onLoginStatusChanged(false);
+    print("Logged out");
   }
 }
